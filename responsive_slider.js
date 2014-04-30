86da3d0ac226f48be48875da77c7d3f9128b75a2
Marmik Desai <marmikdesai@live.com>
2014-05-01 01:20:57 +0530

    bootstrap animation carousel
    bootstrap animation carousel

---
+++ responsive_slider.js
@@ -0,0 +1,33 @@
+/*!
+ * Bootstrap Carousel with Animation
+ * Original author: @Marmik Desai
+ * Further changes, comments: @Marmik Desai
+ * Licensed under the MIT license
+ */
+ 
+(function($) {
+	$.fn.animat = function() {
+		var ts = this;
+		var att;
+		this.find(".carousel-caption").children().each(function() {
+			$(this).css("opacity", 0);
+		});
+		$(".carousel-caption", this.find(".active")).children().each(function() {
+			$(this).css("opacity", 1);
+			if ($(this).data("animate")) {
+				att = $(this).data("animate");
+			}
+			$(this).removeClass(att).addClass("animated" + " " + att);
+		});
+		this.on("slid.bs.carousel", function() {
+			$(this).css("opacity", 1);
+			$(".carousel-caption", ts.find(".active")).children().each(function() {
+				if ($(this).data("animate")) {
+					att = $(this).data("animate");
+				} 
+				$(this).css("opacity", 1).removeClass(att).addClass("animated" + " " + att);
+			});
+		});
+		return this;
+	};
+})(jQuery);
